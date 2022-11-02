3bfa0840de06ae1a3018187c086f28fa4de476d0
Mertensoid <matsoukm@yandex.ru>
2022-11-02 17:06:29 +0300

    Test

--- GBShopUITests/GBShopUITests.swift
+++ GBShopUITests/GBShopUITests.swift
@@ -26,6 +26,8 @@ class GBShopUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        
+        print(app.debugDescription)
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
